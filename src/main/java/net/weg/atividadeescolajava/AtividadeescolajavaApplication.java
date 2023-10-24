86ddf26ccde61661f9dd58b8847096ecd5849992
Thiago Alessandro Batista <tbatista@weg.net>
2023-10-24 20:53:31 -0300

    first commit

---
+++ src/main/java/net/weg/atividadeescolajava/AtividadeescolajavaApplication.java
@@ -0,0 +1,13 @@
+package net.weg.atividadeescolajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeescolajavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeescolajavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/weg/atividadeescolajava/AtividadeescolajavaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.weg.atividadeescolajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeescolajavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
